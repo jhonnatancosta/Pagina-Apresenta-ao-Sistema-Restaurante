9f1002029fc66801c8ffbc8d5e7edf76720be88c
jhonnatancosta <jhonnatan.smartcard@gmail.com>
2025-07-17 00:07:37 -0400

    Add files via upload

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: './', // <--- ESSENCIAL PARA VERCEL ENTENDER CAMINHOS RELATIVOS
   plugins: [react()],
   optimizeDeps: {
     exclude: ['lucide-react'],
