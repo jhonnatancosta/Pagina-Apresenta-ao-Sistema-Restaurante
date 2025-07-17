684015cb9b29f96954e0cb59a1454266e0b42ad1
jhonnatancosta <jhonnatan.smartcard@gmail.com>
2025-07-17 02:05:15 -0400

    Add files via upload

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,20 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import { viteStaticCopy } from 'vite-plugin-static-copy';
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  base: './', // <--- ESSENCIAL PARA VERCEL ENTENDER CAMINHOS RELATIVOS
-  plugins: [react()],
+  base: '/Pagina-Apresenta-ao-Sistema-Restaurante/',
+  plugins: [
+    react(),
+    viteStaticCopy({
+      targets: [
+        {
+          src: 'components/*',   // Copia todos os arquivos dentro da pasta components
+          dest: 'components'     // Para a pasta dist/components
+        }
+      ]
+    }),
+  ],
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
